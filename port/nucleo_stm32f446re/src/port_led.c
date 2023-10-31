a79a0442b982aa087c3ad4a665fab4d914ea25dd
Amadeo de Gracia Herranz <amadeodeg@gmail.com>
2023-10-31 13:15:31 +0100

    NoHAL

--- port/nucleo_stm32f446re/src/port_led.c
+++ port/nucleo_stm32f446re/src/port_led.c
@@ -1,6 +1,9 @@
 #include "port_led.h"
 #include "port_system.h"
-#include "stm32f4xx_hal.h"
+
+/* HW dependent includes */
+#include "stm32f4xx.h"
+
 
 // HW Nucleo-STM32F446RE:
 #define LD2_Pin GPIO_PIN_5
@@ -15,23 +18,17 @@
 #define IDR5_MASK (0x01 << 5) /* Cada pin ocupa 1 bit */
 #define ODR5_MASK (0x01 << 5) /* Cada pin ocupa 1 bit */
 
+
 int port_led_gpio_setup(void)
 {
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-    /* GPIO Ports Clock Enable */
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-    __HAL_RCC_GPIOC_CLK_ENABLE();
-
-    /*Configure GPIO pin : LD2_Pin */
-    GPIO_InitStruct.Pin = LD2_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
-
-    HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+    /* Primero , habilitamos siempre el reloj de los perifericos */
+    RCC -> AHB1ENR |= RCC_AHB1ENR_GPIOAEN ;
+    /* Luego , limpiamos los registros MODER y PUPDR correspondientes */
+    GPIOA -> MODER &= ~ MODER5_MASK ;
+    GPIOA -> PUPDR &= ~ PUPDR5_MASK ;
+    /* Finalmente , configuramos el LED como salida sin pull up/ pull down */
+    GPIOA -> MODER |= MODER5_AS_OUTPUT ;
+    GPIOA -> PUPDR |= PUPDR5_AS_NOPUPD ;
 
     return 0;
 }
--- port/nucleo_stm32f446re/src/port_system.c
+++ port/nucleo_stm32f446re/src/port_system.c
@@ -1,5 +1,10 @@
 #include "port_system.h"
-#include "stm32f4xx_hal.h"
+/* HW dependent includes */
+#include "stm32f4xx.h"
+
+
+/* GLOBAL VARIABLES */
+static volatile uint32_t msTicks = 0; /*!< Variable to store millisecond ticks */
 
 /**
  * @brief System Clock Configuration
@@ -10,43 +15,63 @@
  */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  /** Configure the main internal regulator output voltage */
+  /* Power controller (PWR) */
+  /* Control the main internal voltage regulator output voltage to achieve a trade-off between performance and power consumption when the device does not operate at the maximum frequency */
+  PWR->CR &= ~PWR_CR_VOS; // Clean and set value
+  PWR->CR |= (PWR_CR_VOS & (POWER_REGULATOR_VOLTAGE_SCALE3 << PWR_CR_VOS_Pos));
 
-  /** Configure the main internal regulator output voltage
-   */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+  /* Initializes the RCC Oscillators. */
+  /* Adjusts the Internal High Speed oscillator (HSI) calibration value.*/
+  RCC->CR &= ~RCC_CR_HSITRIM; // Clean and set value
+  RCC->CR |= (RCC_CR_HSITRIM & (RCC_HSI_CALIBRATION_DEFAULT << RCC_CR_HSITRIM_Pos));
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-   * in the RCC_OscInitTypeDef structure.
-   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    while (1)
-      ;
-  }
+  /* RCC Clock Config */
+  /* Initializes the CPU, AHB and APB buses clocks */
+  /* To correctly read data from FLASH memory, the number of wait states (LATENCY)
+      must be correctly programmed according to the frequency of the CPU clock
+      (HCLK) and the supply voltage of the device. */
 
-  /** Initializes the CPU, AHB and APB buses clocks
-   */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  /* Increasing the number of wait states because of higher CPU frequency */
+  FLASH->ACR = FLASH_ACR_LATENCY_2WS; /* Program the new number of wait states to the LATENCY bits in the FLASH_ACR register */
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    while (1)
-      ;
-  }
+  /* Change in clock source is performed in 16 clock cycles after writing to CFGR */
+  RCC->CFGR &= ~RCC_CFGR_SW; // Clean and set value
+  RCC->CFGR |= (RCC_CFGR_SW & (RCC_CFGR_SW_HSI << RCC_CFGR_SW_Pos));
+
+  /* Update the SystemCoreClock global variable */
+  SystemCoreClock = HSI_VALUE >> AHBPrescTable[(RCC->CFGR & RCC_CFGR_HPRE) >> RCC_CFGR_HPRE_Pos];
+
+  /* Configure the source of time base considering new system clocks settings */
+  SysTick_Config(SystemCoreClock / (1000U / TICK_FREQ_1KHZ)); /* Set Systick to 1 ms */
 }
 
 size_t port_system_init()
 {
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+  /* Configure Flash prefetch, Instruction cache, Data cache */
+  /* Instruction cache enable */
+  FLASH->ACR |= FLASH_ACR_ICEN;
 
+  /* Data cache enable */
+  FLASH->ACR |= FLASH_ACR_DCEN;
+
+  /* Prefetch cache enable */
+  FLASH->ACR |= FLASH_ACR_PRFTEN;
+
+  /* Set Interrupt Group Priority */
+  NVIC_SetPriorityGrouping(NVIC_PRIORITY_GROUP_4);
+
+  /* Use systick as time base source and configure 1ms tick (default clock after Reset is HSI) */
+  /* Configure the SysTick IRQ priority. It must be the highest (lower number: 0)*/
+  NVIC_SetPriority(SysTick_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0U, 0U)); /* Tick interrupt priority */
+
+  /* Init the low level hardware */
+  /* Reset and clock control (RCC) */
+  RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN; /* Syscfg clock enabling */
+
+  /* Peripheral clock enable register */
+  RCC->APB1ENR |= RCC_APB1ENR_PWREN; /* PWREN: Power interface clock enable */
   /* Configure the system clock */
   SystemClock_Config();
 
@@ -56,7 +81,7 @@ size_t port_system_init()
 
 uint32_t port_system_get_millis()
 {
-  return HAL_GetTick(); /* ms */
+  return msTicks;
 }
 
 void port_system_delay_ms(uint32_t ms)
@@ -78,18 +103,7 @@ void port_system_delay_until_ms(uint32_t *t, uint32_t ms)
   *t = port_system_get_millis();
 }
 
-// TODO: esta llamada debe sustituirse, la hace HAL_Init. Estaba en stm32f4xx_hal_msp.c
-/**
- * Initializes the Global MSP.
- */
-void HAL_MspInit(void)
-{
 
-  __HAL_RCC_SYSCFG_CLK_ENABLE();
-  __HAL_RCC_PWR_CLK_ENABLE();
-
-  HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_0);
-}
 
 //------------------------------------------------------
 // SUBRUTINAS DE ATENCION A LAS INTERRUPCION
@@ -100,5 +114,5 @@ void HAL_MspInit(void)
 // TODO: sustituir por funcion propia
 void SysTick_Handler(void)
 {
-  HAL_IncTick();
+  msTicks += 1;
 }
