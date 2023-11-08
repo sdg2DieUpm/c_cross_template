9b1c45d18cd2dbc0804b8bdf7254d4c30849656d
Román Cárdenas <rcardenas.rod@gmail.com>
2023-11-08 13:19:52 +0100

    More versatile tests

---
+++ test/unit/stm32f4/test_stm32f4.c
@@ -0,0 +1,38 @@
+#include <unity.h>
+#include "port_led.h"
+#include "stm32f4xx.h" // we can use the definitions from the MCU
+
+void setUp(void)
+{
+    // set stuff up here
+}
+
+void tearDown(void)
+{
+    // clean stuff up here
+}
+
+void test_led(void)
+{
+    TEST_ASSERT_EQUAL(port_led_gpio_setup(), 0);
+    TEST_ASSERT_FALSE(port_led_get());
+
+    TEST_ASSERT_EQUAL(port_led_on(), 0);
+    TEST_ASSERT_TRUE(port_led_get());
+
+    TEST_ASSERT_EQUAL(port_led_off(), 0);
+    TEST_ASSERT_FALSE(port_led_get());
+
+    TEST_ASSERT_EQUAL(port_led_toggle(), 0);
+    TEST_ASSERT_TRUE(port_led_get());
+    TEST_ASSERT_EQUAL(port_led_toggle(), 0);
+    TEST_ASSERT_FALSE(port_led_get());
+}
+
+
+int main(void)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_led);
+    return UNITY_END();
+}
